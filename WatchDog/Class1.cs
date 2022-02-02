37f7bc60c8a3dd1ee9fababf73cb496baafbb1f4
IzyPro <iulelu@appzonegroup.com>
2022-02-02 07:04:08 +0100

    entry point added

--- WatchDog/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace WatchDog
-{
-    public class Class1
-    {
-    }
-}
---
+++ WatchDog/WatchDogExtension.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WatchDog
+{
+    public static class WatchDogExtension
+    {
+        public static IApplicationBuilder UseWatchDog(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<src.WatchDog>();
+        }
+    }
+}
