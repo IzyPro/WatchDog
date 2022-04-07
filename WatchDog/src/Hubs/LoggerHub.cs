e2c5a1572f184fdb40fe3d5a3ab9c69e80dceb4d
khelechy <onyekwerekelechimac@gmail.com>
2022-04-07 16:03:53 +0100

    signalR realtime fetching working perfectly

--- WatchDog/src/Hubs/LoggerHub.cs
+++ WatchDog/src/Hubs/LoggerHub.cs
@@ -1,41 +1,13 @@
 ﻿using Microsoft.AspNetCore.SignalR;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 using WatchDog.src.Helpers;
-using WatchDog.src.Models;
 
 namespace WatchDog.src.Hubs
 {
-    public class LoggerHub : Hub
+    public  class LoggerHub : Hub
     {
-        public async Task GetAllLogs(string id)
-        {
-            var logs = LiteDBHelper.GetAllWatchLogs();
-            if (logs != null)
-                logs.OrderBy(x => x.StartTime);
-            await Clients.All.SendAsync("getAllLogs", logs).ConfigureAwait(false);
-            //await Clients.Client(id).SendAsync("getAllLogs", logs);
-        }
-
-        //public async Task Send(string message1, string Root)
-        //{
-        //    await Clients.All.SendAsync("Send", message1);
-        //}
-
-        //public async void OnChange()
-        //{
-        //    await base.OnConnectedAsync();
-
-        //    //{
-        //    //    LoggerHub loggerHub = new LoggerHub();
-        //    //    await loggerHub.GetAllLogs();
-        //    //}
-        //}
+      
 
     }
 }
