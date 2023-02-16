72cc78b2c87c782396267f7cd8dde5dac0f6bc17
IzyPro <iulelu@appzonegroup.com>
2023-02-16 17:09:56 +0100

    v1.4.4 - fix: dynamic db name for mongo

--- WatchDog/src/Utilities/Constants.cs
+++ WatchDog/src/Utilities/Constants.cs
@@ -1,12 +1,15 @@
-﻿namespace WatchDog.src.Utilities
+﻿using System.Reflection;
+
+namespace WatchDog.src.Utilities
 {
     internal static class Constants
     {
-        public const string WatchDogDatabaseName = "WatchDog_WatchDogDB";
+        public static readonly string DbPrefix = Assembly.GetEntryAssembly().GetName().Name;
+        public static readonly string WatchDogDatabaseName = DbPrefix + "_WatchDogDB";
         public const string WatchLogTableName = "WatchDog_WatchLog";
         public const string WatchLogExceptionTableName = "WatchDog_WatchExceptionLog";
-        public const string LogsTableName = "WatchDog_Logs";
         public const string WatchDogMongoCounterTableName = "WatchDog_Counter";
+        public const string LogsTableName = "WatchDog_Logs";
         public const int PageSize = 20;
     }
 }
